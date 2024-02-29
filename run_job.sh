357e315f4626b15dddfdc5a8e2fb9d8f84cb7476
almaeder <alexander.maeder@gmx.ch>
2024-02-29 22:23:06 +0100

    Benchmark on daint

--- run_job.sh
+++ run_job.sh
@@ -1,23 +1,22 @@
 #!/bin/bash --login 
-#SBATCH --account=hck
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --account=s1212
+#SBATCH --nodes=2
 #SBATCH --constraint=gpu
-#SBATCH --time=05:00:00
-#SBATCH --job-name=test
-#SBATCH --output=run.out
-#SBATCH --error=run.err
+#SBATCH --time=10:00:00
+#SBATCH --job-name=kmc-4node
 
-export OMP_NUM_THREADS=4
+
+export OMP_NUM_THREADS=12
 
 #module swap PrgEnv-cray PrgEnv-gnu/6.0.10
-module load intel-oneapi/2022.1.0
-module load daint-gpu/21.09
-module load cudatoolkit/21.3_11.2
-module swap gcc gcc/9.3.0
+# module load intel-oneapi/2022.1.0
+# module load daint-gpu/21.09
+# module load cudatoolkit/21.3_11.2
+# module swap gcc gcc/9.3.0
 
 #cd ./tests/1-potential/ ; srun -n $SLURM_NTASKS ../../bin/runKMC parameters.txt
 #cd ./tests/2-globaltemp/ ; srun -n $SLURM_NTASKS ../../bin/runKMC parameters.txt
-cd ./tests/3-localtemp/ ; srun -n $SLURM_NTASKS ../../bin/runKMC parameters.txt
+# cd ./tests/3-localtemp/ ; srun -n $SLURM_NTASKS ../../bin/runKMC parameters.txt
 #cd ./tests/prod_2xstructure/ ; srun -n $SLURM_NTASKS ../../bin/runKMC parameters.txt
+cd ./structures/crossbars/40nm_3x3/ ; srun ../../../bin/runKMC parameters.txt
+# cd ./structures/single_devices/timing_10nm/ ; srun  ../../../bin/runKMC parameters.txt
